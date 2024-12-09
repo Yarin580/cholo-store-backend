7a0b047fa87fe060c545658702bfa4b6990ac7b7
Yarin Hershko <yarin5805180@gmail.com>
2024-12-09 22:57:39 +0200

    fix bug

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -1,16 +1,19 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from src.api.tdo.user_dto import UserResponseDto
 from src.application.user.enums import UserRoles
 from src.application.user.services.user import UserService
+from src.modules.db import get_db_session
 from src.modules.jwt import JWTHandler
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="authentication/login")
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserResponseDto:
+async def get_current_user(token: str = Depends(oauth2_scheme),
+                           db_session: AsyncSession = Depends(get_db_session)) -> UserResponseDto:
     """Retrieve the current user from the token."""
     jwt_handler = JWTHandler()
     user_payload = jwt_handler.verify_token(token)
@@ -20,7 +23,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserResponseD
             detail="Invalid or expired token",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    user_service = UserService()
+    user_service = UserService(db_session=db_session)
     user = await user_service.get_by_username(username=user_payload.get("username"))
     if user is None:
         raise HTTPException(
